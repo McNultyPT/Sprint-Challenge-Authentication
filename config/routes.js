da1c9945167c6b2ce038b23e081d4e1b7e5546a5
McNultyPT <mcnultypt@gmail.com>
2019-03-08 08:58:19 -0800

    writing post request for login and creating secrets.js file

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('./routes-model.js');
+const secrets = require('./secrets.js');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -26,15 +28,24 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // let { username, password } = req.body;
+  let { username, password } = req.body;
 
-  // Users.findBy({ username })
-  //   .first()
-  //   .then(user => {
-  //     if (user && bcrypt.compareSync(password, user.password)) {
-  //       const token = 
-  //     }
-  //   })
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function generateToken(user) {
@@ -47,7 +58,7 @@ function generateToken(user) {
     expiresIn: '1d'
   };
 
-  return jwt.sign(payload, authenticate.jwtKey, options);
+  return jwt.sign(payload, secrets.jwtKey, options);
 }
 
 function getJokes(req, res) {
