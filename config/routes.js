105dd92296b00a404b6783210a6111652a1416c4
McNultyPT <mcnultypt@gmail.com>
2019-03-08 08:32:50 -0800

    writing post request to register a user

--- config/routes.js
+++ config/routes.js
@@ -12,7 +12,17 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
