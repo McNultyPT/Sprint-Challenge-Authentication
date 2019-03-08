d447e0378b28b306dca8895b78f1e41ebb5abb38
McNultyPT <mcnultypt@gmail.com>
2019-03-08 09:26:00 -0800

    writing handleChanges function for sign up inputs

--- client/src/signup/SignUp.js
+++ client/src/signup/SignUp.js
@@ -7,31 +7,35 @@ export default class SignUp extends Component {
     };
   
     render() {
-    return (
-      <div>
-        <h2>Sign Up</h2>
-        <form>
-            <label htmlFor='username' />
-                <input
-                    name='username'
-                    id='username'
-                    type='text'
-                    placeholder='Username'
-                    value={this.state.username}
-                />
-            <label htmlFor='password' />
-                <input
-                    name='password'
-                    id='password'
-                    type='text'
-                    placeholder='Password'
-                    value={this.state.password}
-                />
-            <button type='submit'>Sign Up</button>
-        </form>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h2>Sign Up</h2>
+                <form>
+                    <label htmlFor='username' />
+                        <input
+                            name='username'
+                            id='username'
+                            type='text'
+                            placeholder='Username'
+                            value={this.state.username}
+                            onChange={this.handleChanges}
+                        />
+                    <label htmlFor='password' />
+                        <input
+                            name='password'
+                            id='password'
+                            type='text'
+                            placeholder='Password'
+                            value={this.state.password}
+                            onChange={this.handleChanges}
+                        />
+                     <button type='submit'>Sign Up</button>
+                </form>
+            </div>
+        );
+    }
 
-  
+    handleChanges = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
 }
